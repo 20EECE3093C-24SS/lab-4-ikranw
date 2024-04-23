d74e1a9e10adb2a26a3e5a3889d07c9aa4b52148
Ikran Warsame <141252436+ikranw@users.noreply.github.com>
2024-04-23 15:58:04 -0400

    Update course_grader.py

--- sample/course_grader.py
+++ sample/course_grader.py
@@ -25,11 +25,11 @@ def convert_to_letter_grade(score):
 
     if score >= 90:
         return 'A'
-    elif score >= 80:
+    elif score >= 80 and score <= 89:
         return 'B'
-    elif score >= 70:
+    elif score >= 70 and score <= 79:
         return 'C'
-    elif score >= 60:
+    elif score >= 60 and score <= 69:
         return 'D'
     else:
         return 'F'
