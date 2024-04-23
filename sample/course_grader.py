171f8b27aa785f62eb12497652b976bac913b95b
Ikran Warsame <141252436+ikranw@users.noreply.github.com>
2024-04-23 16:05:54 -0400

    Update course_grader.py

--- sample/course_grader.py
+++ sample/course_grader.py
@@ -18,7 +18,7 @@ def convert_to_letter_grade(score):
         >>> convert_to_letter_grade(64)
         'D'
     """
-      if not isinstance(score, (int, float)):
+    if not isinstance(score, (int, float)):
         raise TypeError("Score must be a numeric value.")
     if score < 0 or score > 100:
         raise ValueError("Score must be between 0 and 100.")
